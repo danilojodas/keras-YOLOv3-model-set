bd5ffe36b84ebb310b76627eba31827408506c5d
Danilo Jodas <jodas@cupertino.recogna.tech>
2020-08-08 09:01:25 -0300

    Fixed bugs

---
+++ exec.sh
@@ -0,0 +1,1 @@
+nohup python train.py --model_type=yolo3_mobilenet_lite --anchors_path=configs/yolo3_anchors.txt --classes_path=configs/my_classes.txt --annotation_file=trainval.txt --val_annotation_file=valid.txt --eval_online --save_eval_checkpoint --total_epoch 300 --batch_size 64 > output.log &
