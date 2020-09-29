1b07536501f7b3464a878731fd8932a4d78dc923
Danilo Jodas <jodas@cupertino.recogna.tech>
2020-09-29 09:58:48 -0300

    Added new mobilenet with batch size 4

--- exec.sh
+++ exec.sh
@@ -5,4 +5,6 @@
 # No data augmentation
 #nohup python train.py --model_type=yolo3_mobilenet_lite_spp --anchors_path=configs/yolo3_anchors.txt --classes_path=configs/my_classes.txt --annotation_file=trainval_no_data_aug.txt --val_annotation_file=valid.txt --eval_online --save_eval_checkpoint --total_epoch 300 --batch_size 64 > output.log &
 
-nohup python train.py --model_type=yolo3_darknet --anchors_path=configs/yolo3_anchors.txt --classes_path=configs/my_classes.txt --annotation_file=trainval.txt --val_annotation_file=valid.txt --eval_online --gpu_num 0 --total_epoch 300 --batch_size 4 > output.log &
+#nohup python train.py --model_type=yolo3_darknet --anchors_path=configs/yolo3_anchors.txt --classes_path=configs/my_classes.txt --annotation_file=trainval.txt --val_annotation_file=valid.txt --eval_online --gpu_num 0 --total_epoch 300 --batch_size 4 > output.log &
+
+nohup python train.py --model_type=yolo3_mobilenet_lite --anchors_path=configs/yolo3_anchors.txt --classes_path=configs/my_classes.txt --annotation_file=trainval.txt --val_annotation_file=valid.txt --eval_online --save_eval_checkpoint --total_epoch 300 --batch_size 4 > output.log &
