77dca10ea4c61308455f2296984292840863102e
Danilo Jodas <jodas@cupertino.recogna.tech>
2020-09-30 18:40:19 -0300

    Added model v6

--- exec.sh
+++ exec.sh
@@ -9,4 +9,4 @@
 
 #nohup python train.py --model_type=yolo3_mobilenet_lite --anchors_path=configs/yolo3_anchors.txt --classes_path=configs/my_classes.txt --annotation_file=trainval.txt --val_annotation_file=valid.txt --eval_online --save_eval_checkpoint --total_epoch 300 --batch_size 4 > output.log &
 
-nohup python train.py --model_type=yolo3_mobilenet_lite_spp --anchors_path=configs/yolo3_anchors.txt --classes_path=configs/my_classes.txt --annotation_file=trainval.txt --val_annotation_file=valid.txt --eval_online --save_eval_checkpoint --total_epoch 300 --batch_size 4 > output.log &
+nohup python train.py --model_type=yolo3_mobilenet_lite_spp --anchors_path=configs/yolo3_anchors.txt --classes_path=configs/my_classes.txt --annotation_file=trainval.txt --val_annotation_file=valid.txt --eval_online --save_eval_checkpoint --total_epoch 300 --batch_size 4 --transfer_epoch 50 > output.log &
